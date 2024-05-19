62786752c9ee8ca4f024797f54d8549bf8e77837
Gabriel Perez <gabpalk@gmail.com>
2024-05-19 19:08:08 -0300

    Include usecase code for grabbing content and about repository (#4)

--- app/src/main/java/com/gabcode/compassabout/data/AboutRepository.kt
+++ app/src/main/java/com/gabcode/compassabout/data/AboutRepository.kt
@@ -1,17 +1,50 @@
 package com.gabcode.compassabout.data
 
+import android.util.Log
 import com.gabcode.compassabout.data.database.AboutDao
+import com.gabcode.compassabout.data.database.AboutEntity
+import com.gabcode.compassabout.util.ConnectivityChecker
+import com.gabcode.compassabout.util.NotFoundConnectivityException
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
 interface IAboutRepository {
     suspend fun fetchContent(): Result<AboutResponse>
 }
+
 class AboutRepository(
     private val service: IAboutService,
+    private val connectivityChecker: ConnectivityChecker,
     private val aboutDao: AboutDao,
     private val ioDispatcher: CoroutineDispatcher
 ) : IAboutRepository {
-    override suspend fun fetchContent(): Result<AboutResponse> {
-        TODO("Not yet implemented")
+
+    override suspend fun fetchContent(): Result<AboutResponse> = withContext(ioDispatcher) {
+        try {
+            val result = getCachedData()?.let { entity ->
+                AboutResponse(entity.content)
+            } ?: getRemoteData()
+
+            Result.success(result)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
+
+    private suspend fun getRemoteData(): AboutResponse {
+        if (!connectivityChecker.isNetworkAvailable()) {
+            throw NotFoundConnectivityException()
+        }
+
+        return service.makeRequest().also { response ->
+            saveRemoteData(response)
+        }
+    }
+
+    private suspend fun saveRemoteData(response: AboutResponse) {
+        aboutDao.insert(content = AboutEntity(content = response.content))
+        Log.i("AboutRepository", "Data saved to database")
+    }
+
+    private suspend fun getCachedData(): AboutEntity? = aboutDao.getData()
 }
