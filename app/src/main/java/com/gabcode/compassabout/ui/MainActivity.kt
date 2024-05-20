692bd8c4e5b0e7dca0c47f95d2618590432ba33f
Gabriel Perez <gabpalk@gmail.com>
2024-05-20 19:19:50 -0300

    contemplate configuration changes.

--- app/src/main/java/com/gabcode/compassabout/ui/MainActivity.kt
+++ app/src/main/java/com/gabcode/compassabout/ui/MainActivity.kt
@@ -8,12 +8,8 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.gabcode.compassabout.R
 import com.gabcode.compassabout.databinding.ActivityMainBinding
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity(), Navigator {
 
@@ -37,7 +33,9 @@ class MainActivity : AppCompatActivity(), Navigator {
 
         viewModel.fetchContent()
 
-        navigate(NavigatorRoute.HOME)
+        savedInstanceState?.getString(CURRENT_ROUTE)?.let { route ->
+            navigate(NavigatorRoute.valueOf(route))
+        } ?: navigate(NavigatorRoute.HOME)
     }
 
     private fun loadFragment(fragment: Fragment) {
@@ -52,5 +50,18 @@ class MainActivity : AppCompatActivity(), Navigator {
             NavigatorRoute.HOME -> loadFragment(HomeFragment.newInstance())
             NavigatorRoute.ABOUT -> loadFragment(AboutListFragment.newInstance())
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        val currentFragment = supportFragmentManager.fragments.firstOrNull()
+        if (currentFragment is AboutListFragment) {
+            outState.putString(CURRENT_ROUTE, NavigatorRoute.ABOUT.name)
+        }
+    }
+
+    companion object {
+        private const val CURRENT_ROUTE = "current_route"
     }
 }
