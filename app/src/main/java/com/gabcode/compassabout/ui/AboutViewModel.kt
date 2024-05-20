c065788b3ba562c512a6c2205ff41cbce6b14c85
Gabriel Perez <gabpalk@gmail.com>
2024-05-20 01:29:50 -0300

    add viewmodel factory and ui for lists

---
+++ app/src/main/java/com/gabcode/compassabout/ui/AboutViewModel.kt
@@ -0,0 +1,86 @@
+package com.gabcode.compassabout.ui
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gabcode.compassabout.domain.FindCharSequenceOccurrencesUseCase
+import com.gabcode.compassabout.domain.FindEveryTenthCharacterUseCase
+import com.gabcode.compassabout.domain.GrabContentUseCase
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class AboutViewModel(
+    private val grabContentUseCase: GrabContentUseCase,
+    private val findEveryTenthCharacterUseCase: FindEveryTenthCharacterUseCase,
+    private val findCharSequenceOccurrencesUseCase: FindCharSequenceOccurrencesUseCase
+) : ViewModel() {
+
+    private val mWordOccurrences = MutableStateFlow<UIState<List<WordOccurrenceItem>>>(UIState.Idle)
+    val wordOccurrences: StateFlow<UIState<List<WordOccurrenceItem>>> = mWordOccurrences
+
+    private val mTenthCharacters = MutableStateFlow<UIState<List<TenthCharacterItem>>>(UIState.Idle)
+    val tenthCharacters: StateFlow<UIState<List<TenthCharacterItem>>> = mTenthCharacters
+
+    fun fetchContent() {
+        viewModelScope.launch(CoroutineName("fetchContent")) {
+            Log.d("AboutViewModel", "fetching content...")
+            grabContentUseCase.invoke()
+        }
+    }
+
+    fun startProcessingData() {
+        with(viewModelScope) {
+            Log.d("AboutViewModel", "start processing data...")
+            launch(CoroutineName("findTenthCharacters")) { findTenthCharacters() }
+            launch(CoroutineName("findWordOccurrences")) { findWordOccurrences() }
+        }
+    }
+
+    private suspend fun findTenthCharacters() {
+        mTenthCharacters.value = UIState.Loading
+        findEveryTenthCharacterUseCase.invoke().collect { result ->
+            if (result.isFailure) {
+                mTenthCharacters.update {
+                    val exception = result.exceptionOrNull() ?: IllegalStateException("Unknown error")
+                    UIState.Error(exception)
+                }
+                return@collect
+            }
+
+            result.getOrNull()?.let { data ->
+                val uiModel = data.toUIModel()
+                Log.i("AboutViewModel", "findTenthCharacters result: $uiModel")
+
+                mTenthCharacters.update {
+                    UIState.Success(uiModel)
+                }
+            }
+        }
+    }
+
+    private suspend fun findWordOccurrences() {
+        mWordOccurrences.value = UIState.Loading
+        findCharSequenceOccurrencesUseCase.invoke().collect { result ->
+            if (result.isFailure) {
+                mWordOccurrences.update {
+                    val exception =
+                        result.exceptionOrNull() ?: IllegalStateException("Unknown error")
+                    UIState.Error(exception)
+                }
+                return@collect
+            }
+
+            result.getOrNull()?.let { data ->
+                val uiModel = data.toUIModel()
+                Log.i("AboutViewModel", "findOccurrences result: $uiModel")
+
+                mWordOccurrences.update {
+                    UIState.Success(uiModel)
+                }
+            }
+        }
+    }
+}
